1f0f299a4f4dc48693ac9c72068f24615800aa0c
ovr <orsuvenkatesh2021@gmail.com>
2023-03-20 22:36:45 +0530

    put request added

--- src/main/java/com/example1/service/ProductsServiceImpl.java
+++ src/main/java/com/example1/service/ProductsServiceImpl.java
@@ -3,7 +3,7 @@ package com.example1.service;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +12,7 @@ import com.example1.model.Products;
 import com.example1.repository.ProductsRepo;
 
 @Service
-public   class ProductsServiceImpl implements ProductsService {
+public abstract   class ProductsServiceImpl implements ProductsService {
 
 @Autowired
 ProductsRepo productrepo;
@@ -23,7 +23,7 @@ public Products saveProduct(Products product) {
 	}
 
 @Override
-public void remove(int pid) {
+public void remove(Integer pid) {
 	productrepo.deleteById(pid);
 }
 @Override
@@ -33,8 +33,18 @@ public List<Products> getProducts(){
 	return productdetails;
 }
 @Override
-public Products getOne(int pid) {
-return productrepo.findById(pid).get();
-
+public Products getOne(Integer pid) {
+    return productrepo.findById(pid).get();
 }
+@Override
+public Products updateOne(Products product,Integer pid) {
+Optional <Products> optional=productrepo.findById(pid);
+	Products p1=optional.get();
+	p1.setPid(pid);
+	p1.setPname(product.getPname());
+	p1.setPprice(product.getPprice());
+	return productrepo.save(p1);
+}
+
+
 }
